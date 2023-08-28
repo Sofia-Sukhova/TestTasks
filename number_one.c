5989e895e7f7af9598be8569fcc5726d909d2749
Sofia Sukhova <sukhova.si@phystech.edu>
2023-08-28 19:04:23 +0300

    Beautification v.1 done

--- number_one.c
+++ number_one.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 
+//aфункция распечатывает число побитово
 void bit_print(unsigned long long N){
 	unsigned long long tmp = 0;
 	int length = 0;
@@ -21,7 +22,7 @@ void bit_print(unsigned long long N){
 	printf("\n");
 }
 
-
+//функция считывает число, записанное побитово
 unsigned long long  bit_scan(){
 	unsigned long long read = 0;
 	char symbol;
@@ -42,18 +43,21 @@ int main(){
 	unsigned long long N = 0, M = 0, New = 0;
 	int i1 = 0, i2 = 0, length;
 
-
+	//считывание входных данных задачи
 	N = bit_scan();
 	M = bit_scan();
 	scanf("%d%d", &i1, &i2);
 	length = i2 - i1 + 1;
 
+	//новое число берет старшие разряды из N (c i2 и старше)
 	New = N >> (i2 + 1);
+	//дописываются соответствующие разряды из M (с i1 до i2)
 	New = (New << length) + (M & ((1 << length) - 1));
+	//дописываются оставшиеся младшие разряды из N (c i1 и младше)
 	New = (New << i1) + (N & ((1 << i1) - 1));
 
 	bit_print(New);
-	
+	return 0;
 }
 
 
--- number_two.c
+++ number_two.c
@@ -1,6 +1,18 @@
+// Основная идея: 
+//  Граф проходится (DFS) в двух направлениях - от вершины 1 до N с пометкой в статусе вершины о посещении
+//  Затем граф проходится в обратном направлении - от вершины N до 1 с пометкой в статусе вершины о посещении
+//  Вершины, имеющие status < 2 - мертвые, т.к. посещались менее 2-х раз. Такие вершины удаляются
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//узел списка, необходимый для хранения взаимосвязей в графе. 
+//  vertex - идентификатор вершины либо 0xdeadfa11 для головы списка
+//  linked - указывает на элемент списка, соответствующий данному ребру графа, но с началом в другой вершине
+//  nextNode - следующий элемент списка
+//  prevNode - предыдущий элемент списка
 struct listNode{
     int vertex;
     struct listNode * linked;
@@ -8,6 +20,11 @@ struct listNode{
     struct listNode * prevNode;
 };
 
+//вершина графа
+//  vertex - идентификатор вершины
+//  status - необходим для выполнения алгоритма, маркирует "живость" вершины
+//  nextHead - указатель на голову списка соседних вершин, достижимых из данной
+//  prevHead - указатель на голову списка соседних вершин, из которых достижима данная
 struct node {
     int vertex;
     int status;
@@ -15,6 +32,12 @@ struct node {
     struct listNode * prevHead;
 };
 
+//граф
+//  size - размер графа, общее число вершин
+//  numVertices - число живых вершин
+//  numEdges - число ребер графа
+//  visited - массив посещенных вершин для реализации DFS
+//  nodes - массив указателей на вершины графа
 struct Graph {
     int size;
     int numVertices;
@@ -23,13 +46,7 @@ struct Graph {
     struct node** nodes;
 };
 
-void assignListNode(struct listNode * assigned, int vertex, struct listNode * linked, struct listNode * nextNode, struct listNode * prevNode){
-    assigned -> vertex = vertex;
-    assigned -> linked = linked;
-    assigned -> nextNode = nextNode;
-    assigned -> prevNode = prevNode;
-}
-
+//создает пустой узел списка
 struct listNode* createListNode(){
     struct listNode * newNode = calloc(1, sizeof(struct listNode));
     newNode -> vertex = 0;    
@@ -39,6 +56,15 @@ struct listNode* createListNode(){
     return newNode; 
 }
 
+//идентифицирует существующий узел списка переданными значениями 
+void assignListNode(struct listNode * assigned, int vertex, struct listNode * linked, struct listNode * nextNode, struct listNode * prevNode){
+    assigned -> vertex = vertex;
+    assigned -> linked = linked;
+    assigned -> nextNode = nextNode;
+    assigned -> prevNode = prevNode;
+}
+
+//помещает в список элемент pushed после элемента prevInList
 void pushInList(struct listNode* pushed, struct listNode * prevInList){
     pushed -> nextNode = prevInList -> nextNode;
     pushed -> prevNode = prevInList;
@@ -48,7 +74,7 @@ void pushInList(struct listNode* pushed, struct listNode * prevInList){
     prevInList -> nextNode = pushed;
 }
 
-// Создание вершины
+// Создает вершину графа с данным идентификатором вершины
 struct node* createNode(int v) {
     struct node* newNode = calloc(1, sizeof(struct node));
     newNode -> vertex = v;
@@ -60,14 +86,14 @@ struct node* createNode(int v) {
     return newNode;
 }
 
-// Создание графа
+//  Создает граф с заданным числом вершин
 struct Graph* createGraph(int vertices, int edges) {
     struct Graph* graph = calloc(1, sizeof(struct Graph));
     graph -> size = vertices;
     graph -> numVertices = vertices;
     graph -> numEdges = edges;
     graph -> nodes = calloc(vertices, sizeof(struct node*));
-    graph -> visited = calloc(vertices, sizeof(int)); //  ???????????
+    graph -> visited = calloc(vertices, sizeof(int));
  
     int i;
     for (i = 0; i < vertices; i++) {
@@ -77,7 +103,21 @@ struct Graph* createGraph(int vertices, int edges) {
     return graph;
 }
  
-// Выводим граф
+//  Считывает граф в формате задачи
+struct Graph* scanGraph(){
+    int N = 0, M = 0;
+    int src, dest;
+    scanf("%d %d", &N, &M);
+    struct Graph * graph = createGraph(N, M);
+    for (int i =0; i < M; i++){
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src - 1, dest - 1);
+    }
+
+    return graph;
+}
+
+//  Выводит графа в формате, заданном в задаче
 void printGraph(struct Graph* graph) {
     int v;
     printf("%d\n%d\n", graph -> numVertices, graph -> numEdges);
@@ -93,39 +133,36 @@ void printGraph(struct Graph* graph) {
     }
 }
 
-// naming of vars!!!!!!!!!!!
+//  Добавляет новое ребро src -> dest в граф graph . Ребра в список исходящих вершин добавляются в порядке возрастания идентификатора ребра
 void addEdge(struct Graph* graph, int src, int dest) {
     struct node * source = graph -> nodes[src];
     struct node * destination = graph -> nodes[dest];
 
-    struct listNode * newNext = createListNode();
-    struct listNode * newPrev = createListNode();
-    //
+    struct listNode * destNewListNode = createListNode();
+    struct listNode * srcNewListNode = createListNode();
+    
+    //поиск позиции в списке, в которую следует поместить новый узел
     struct listNode* temp = source -> nextHead;
     while (temp -> nextNode != NULL && (temp -> vertex < dest || temp -> vertex == 0xdeadfa11)){
         temp = temp -> nextNode;
     }
-    if (temp != source -> nextHead){
-        if (temp -> vertex < dest){
-            assignListNode(newNext, destination -> vertex, newPrev, NULL, NULL);
-            pushInList(newNext, temp);
+    //добавление новой вершины (vertex == dest) в список вершин, достижимых из source
+        if (temp -> vertex < dest || temp == source -> nextHead){
+            assignListNode(destNewListNode, dest, srcNewListNode, NULL, NULL);
+            pushInList(destNewListNode, temp);
         } else {
-            assignListNode(newNext, destination -> vertex, newPrev, NULL, NULL);
-            pushInList(newNext, temp -> prevNode);
+            assignListNode(destNewListNode, dest, srcNewListNode, NULL, NULL);
+            pushInList(destNewListNode, temp -> prevNode);
 
         }
-    } else {
-        assignListNode(newNext, destination -> vertex, newPrev, NULL, NULL);
-        pushInList(newNext, source -> nextHead);
-    } 
 
-    //
-    assignListNode(newPrev, source -> vertex, newNext, NULL, NULL);
-    pushInList(newPrev, destination -> prevHead);
+    //добавление новой вершины (vertex == src) в список вершин, из которых достижима destination
+    assignListNode(srcNewListNode, src, destNewListNode, NULL, NULL);
+    pushInList(srcNewListNode, destination -> prevHead);
 }
  
 
-
+//  Удаляет узел списка (с удалением привязанного к нему элемента в другом списке
 void deleteListNode(struct listNode * deleted){
     struct listNode * linked = deleted -> linked;
 
@@ -146,34 +183,35 @@ void deleteListNode(struct listNode * deleted){
     free(deleted);
 }
 
-//
+//  Удаляет вершину графа
 void deleteNode(struct Graph * graph, int vertex){
 
-    //delete previous 
+    //delete list of previous
     struct node *deleted = graph -> nodes[vertex];
     struct listNode * temp = deleted -> prevHead -> nextNode;
     while (temp != NULL){
         deleteListNode(temp);
-        (graph -> numEdges) --;
+        (graph -> numEdges) -= 1;
         temp = deleted -> prevHead -> nextNode;
     }
     free(deleted -> prevHead);
 
-    //delete next
+    //delete list of next
     temp = deleted -> nextHead -> nextNode;
     while (temp != NULL){
         deleteListNode(temp);
-        (graph -> numEdges) --;
+        (graph -> numEdges) -= 1;
         temp = deleted -> nextHead -> nextNode;      
     }
     free(deleted -> nextHead);
 
-    //graph quest?
+    //delete pointer to node in graph
     graph -> nodes[vertex] = NULL;
 
     free(deleted);
 }
 
+//  Удаляет граф
 void deleteGraph(struct Graph * graph){
     for (int i = 0; i < graph -> size; i ++){
         if (graph -> nodes[i] != NULL){
@@ -183,6 +221,11 @@ void deleteGraph(struct Graph * graph){
     free(graph);
 }
 
+
+//  Осуществляет поиск в глубину в заданном направлении.
+//      direction == 1 - по направлению ребер графа
+//      direction == 0 - против направления ребер графа
+//      при обходе делается пометка о статусе "посещенности" в вершине. 
 void DFS(struct Graph* graph, int vertex, int direction) {
 
     struct listNode* temp = NULL;
@@ -203,38 +246,30 @@ void DFS(struct Graph* graph, int vertex, int direction) {
     }
 }
 
+// Очищает список посещенных вершин в графе, заполнямый по ходу DFS
 void clearVisitedList(struct Graph * graph) {
     for (int i = 0; i < graph -> size; i++){
         graph -> visited[i] = 0;
     }
 }
 
+//Удаляет мертвые вершины по принципу:
+//      если статус меньше двух (вершина посещена менее двух раз), то вершина - мертвая
 void deleteDead(struct Graph * graph) {
     for (int i = 1; i < (graph -> size) - 1; i++){
         if (graph -> nodes[i] -> status < 2){
             deleteNode(graph, i);
-            (graph -> numVertices) --;
+            (graph -> numVertices) -= 1;
         }
     }
 }
 
-struct Graph* scanGraph(){
-    int N = 0, M = 0;
-    int src, dest;
-    scanf("%d %d", &N, &M);
-    struct Graph * graph = createGraph(N, M);
-    for (int i =0; i < M; i++){
-        scanf("%d %d", &src, &dest);
-        addEdge(graph, src - 1, dest - 1);
-    }
-
-    return graph;
-}
-
 int main() { 
     struct Graph* graph =  scanGraph();
-    // printGraph(graph);
 
+    //  Граф проходится в двух направлениях - от вершины 1 до N с пометкой в статусе вершины о посещении
+    //  Затем граф проходится в обратном направлении - от вершины N до 1 с пометкой в статусе вершины о посещении
+    //  Вершины, имеющие status < 2 - мертвые, т.к. посещались менее 2-х раз. Такие вершины удаляются
     DFS (graph, 0, 1);
     clearVisitedList(graph);
     DFS (graph, (graph -> size) - 1, 0);
